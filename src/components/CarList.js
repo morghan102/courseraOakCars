4ca8a109960ac84b25d7ee023479a15b69662d73
Morghan <morghan102@gmail.com>
2021-06-28 15:10:31 -0700

    Added state

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,9 +1,22 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class CarList extends Component {
+    constructor(){
+        super();
+        this.state = { carList: [] };
+    }
+
+    componentDidMount() { // initial data loading and 1-time processes like getting a list.
+        axios.get('https://givecars.herokuapp.com') //this returns a promise
+        .then((response) => {
+            this.setState({carList: response.data});
+        });
+    }
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Car List</Text>
